f7525616256571ada82766d5c4223db1833f493a
PeeHaa <info@pieterhordijk.com>
2012-02-17 00:17:14 +0100

    Implemented options page Implemented background page Added support for multiple tags The icon is now displayed  if the current page has questions Cleaned up code

--- cv-pls/script.js
+++ cv-pls/script.js
@@ -1,12 +1,19 @@
 (function() {
-  if ($('#questions').length) {
-    $('.question-summary').each(function() {
-      if ($('.tags', this).hasClass('t-php')) {
-        var question = $(this);
-        var url = $('h3 a', question).attr('href');
+  chrome.extension.sendRequest({method: "getLocalStorage", key: "tags"}, function(response) {
+    var tags_string = response.data;
+    var tags_array = tags_string.split(' ');
 
-        question.html('<a href="#" class="post-tag">cv-pls</a> ' + 'http://stackoverflow.com' + url);
-      }
-    });
-  }
+    if ($('#questions').length) {
+      chrome.extension.sendRequest({method: "showIcon"}, function(response) {});
+
+      $('.question-summary').each(function() {
+        if ($('.tags', this).is('.' + tags_array.join(', .'))) {
+          var question = $(this);
+          var url = $('h3 a', question).attr('href');
+
+          question.html('<a href="/questions/tagged/cv-pls" class="post-tag">cv-pls</a> ' + 'http://stackoverflow.com' + url);
+        }
+      });
+    }
+  });
 })(jQuery);
