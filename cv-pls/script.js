440898262a62bc2344950f873e8b6068a35961e7
PeeHaa <info@pieterhordijk.com>
2012-02-17 01:02:54 +0100

    Implemented (not yet functioning) cv-pls request to chat on questions in vote menu

--- cv-pls/script.js
+++ cv-pls/script.js
@@ -3,7 +3,8 @@
     var tags_string = response.data;
     var tags_array = tags_string.split(' ');
 
-    if ($('#questions').length) {
+    // page with questions overview
+    if ($('body.questions-page').length) {
       chrome.extension.sendRequest({method: "showIcon"}, function(response) {});
 
       $('.question-summary').each(function() {
@@ -12,6 +13,23 @@
           var url = $('h3 a', question).attr('href');
 
           question.html('<a href="/questions/tagged/cv-pls" class="post-tag">cv-pls</a> ' + 'http://stackoverflow.com' + url);
+        }
+      });
+    }
+
+    // page of question
+    if ($('body.question-page').length) {
+      chrome.extension.sendRequest({method: "showIcon"}, function(response) {});
+
+      var question = $('#question');
+      $('.post-taglist .post-tag', question).each(function() {
+        var normalized_tag = 't-' + $(this).html();
+        var length = tags_array.length;
+
+        for(var i = 0; i < length; i++) {
+          if (normalized_tag == tags_array[i]) {
+            $('.post-menu', question).append('<span class="lsep">|</span><a href="#">cv-pls in chat</a>');
+          }
         }
       });
     }
