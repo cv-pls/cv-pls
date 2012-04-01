1a00262e6ed2e298b2588ab46763c6498817b5e3
PeeHaa <info@pieterhordijk.com>
2012-04-01 20:01:57 +0200

    Added close status and polling settings

--- cv-pls/settings.js
+++ cv-pls/settings.js
@@ -13,6 +13,25 @@ function SettingsManager(pluginSettings) {
     if (pluginSettings.oneBoxHeight() !== null) {
       $('input[name="oneboxheight"]').val(pluginSettings.oneBoxHeight());
     }
+
+    if (pluginSettings.showCloseStatus()) {
+      $('input[name="status"]').prop('checked', 'checked');
+    } else {
+      $('input[name="poll"]').attr('disabled', true);
+      $('input[name="pollinterval"]').attr('disabled', true);
+    }
+
+    if (pluginSettings.pollCloseStatus()) {
+      $('input[name="poll"]').prop('checked', 'checked');
+    } else {
+      $('input[name="pollinterval"]').attr('disabled', true);
+    }
+
+    if (pluginSettings.pollInterval() === null) {
+      $('input[name="pollinterval"]').val('5');
+    } else {
+      $('input[name="pollinterval"]').val(pluginSettings.pollInterval());
+    }
   }
 }
 
@@ -31,13 +50,46 @@ function SettingsManager(pluginSettings) {
     settings.saveSetting('oneBox', checked);
 
     if (checked) {
-        $(':input[name="oneboxheight"]').removeAttr('disabled');
+      $(':input[name="oneboxheight"]').removeAttr('disabled');
     } else {
-        $(':input[name="oneboxheight"]').attr('disabled', true);
+      $(':input[name="oneboxheight"]').attr('disabled', true);
     }
   });
 
   $('input[name="oneboxheight"]').keyup(function() {
     settings.saveSetting('oneBoxHeight', $(this).val());
+  });
+
+  $('input[name="status"]').change(function() {
+    var checked = $(this).prop('checked');
+    settings.saveSetting('showCloseStatus', checked);
+
+    if (checked) {
+      var $poll = $('input[name="poll"]');
+      $poll.removeAttr('disabled');
+      if ($($poll).prop('checked')) {
+        $(':input[name="pollinterval"]').removeAttr('disabled');
+      } else {
+        $(':input[name="pollinterval"]').attr('disabled', true);
+      }
+    } else {
+      $(':input[name="poll"]').attr('disabled', true);
+      $(':input[name="pollinterval"]').attr('disabled', true);
+    }
+  });
+
+  $('input[name="poll"]').change(function() {
+    var checked = $(this).prop('checked');
+    settings.saveSetting('pollCloseStatus', checked);
+
+    if (checked) {
+      $('input[name="pollinterval"]').removeAttr('disabled');
+    } else {
+      $('input[name="pollinterval"]').attr('disabled', true);
+    }
+  });
+
+  $('input[name="pollinterval"]').keyup(function() {
+    settings.saveSetting('pollInterval', $(this).val());
   });
 })(jQuery);
