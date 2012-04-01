27ed9e1264fe024a150687b5c74a2316c1a358b1
PeeHaa <info@pieterhordijk.com>
2012-04-01 16:43:30 +0200

    Base setup of refactored settings manager

--- cv-pls/settings.js
+++ cv-pls/settings.js
@@ -1,260 +1,35 @@
-function SettingsManager(settings) {
-  var self = this;
-
-  this.settings = settings;
-
+function SettingsManager(pluginSettings) {
   this.init = function() {
-    if (settings.isSoundEnabled()) {
-      $('input[name="beep"]').prop('checked', 'checked');
-    }
-
-    if (settings.isAvatarEnabled()) {
-      $('input[name="avatar"]').prop('checked', 'checked');
-    }
-
-    if (settings.isAvatarOnloadEnabled()) {
-      $('input[name="avataronload"]').prop('checked', 'checked');
-    }
-
-    if (settings.isIconEnabled()) {
+    if (pluginSettings.showIcon()) {
       $('input[name="icon"]').prop('checked', 'checked');
     }
 
-    if (settings.isCvOneboxEnabled()) {
+    if (pluginSettings.oneBox()) {
       $('input[name="oneboxcv"]').prop('checked', 'checked');
+    } else {
+      $('input[name="oneboxheight"]').attr('disabled', true);
     }
-
-    if (settings.isDelvOneboxEnabled()) {
-      $('input[name="oneboxdelv"]').prop('checked', 'checked');
-    }
-
-    $('input[name="oneboxheight"]').val(settings.getOneboxHeight());
-
-    $('select[name="oneboxheightunits"]').val(settings.getOneboxHeightUnit());
-
-    if (settings.isCvStatusEnabled()) {
-      $('input[name="status"]').prop('checked', 'checked');
-    }
-
-    if (settings.isCvReasonEnabled()) {
-      $('input[name="reason"]').prop('checked', 'checked');
-    }
-
-    if (settings.isPollEnabled()) {
-      $('input[name="poll"]').prop('checked', 'checked');
-    }
-
-    $('input[name="pollinterval"]').val(settings.getPollInterval());
-  }
-
-  this.saveSetting = function(key, value) {
-    settings.saveSetting(key, value);
-  }
-}
-
-// database class to get and save settings
-function Settings() {
-  var self = this;
-
-  this.saveSetting = function(key, value) {
-    localStorage.setItem(key, value);
-  }
-
-  this.getSetting = function(key) {
-    return localStorage.getItem(key);
-  }
-
-  this.deleteSetting = function(key) {
-    localStorage.remove(key);
-  }
-
-  this.truncate = function() {
-    localStorage.clear();
-  }
-
-  this.isSoundEnabled = function() {
-    if (self.getSetting('sound-notification') == 'true') {
-      return true;
-    }
-
-    return false;
-  }
-
-  this.isAvatarEnabled = function() {
-    if (self.getSetting('avatar-notification') == 'true') {
-      return true;
-    }
-
-    return false;
-  }
-
-  this.isAvatarOnloadEnabled = function() {
-    if (self.getSetting('avatar-onloadnotification') == 'true') {
-      return true;
-    }
-
-    return false;
-  }
-
-  this.isIconEnabled = function() {
-    var value = self.getSetting('icon-enabled');
-    if (value == 'true' || value === null) {
-      return true;
-    }
-
-    return false;
-  }
-
-  this.isCvOneboxEnabled = function() {
-    var value = self.getSetting('cv-onebox');
-    if (value == 'true' || value === null) {
-      return true;
-    }
-
-    return false;
-  }
-
-  this.isDelvOneboxEnabled = function() {
-    var value = self.getSetting('delv-onebox');
-    if (value == 'true' || value === null) {
-      return true;
-    }
-
-    return false;
-  }
-
-  this.getOneboxHeight = function() {
-    var value = self.getSetting('onebox-height');
-
-    return value;
-  }
-
-  this.getOneboxHeightUnit = function() {
-    var value = self.getSetting('onebox-heightunit');
-
-    return value;
-  }
-
-  this.isCvStatusEnabled = function() {
-    var value = self.getSetting('cv-status');
-    if (value == 'true' || value === null) {
-      return true;
-    }
-
-    return false;
-  }
-
-  this.isCvReasonEnabled = function() {
-    var value = self.getSetting('cv-reason');
-    if (value == 'true') {
-      return true;
-    }
-
-    return false;
-  }
-
-  this.isPollEnabled = function() {
-    var value = self.getSetting('poll');
-    if (value == 'true') {
-      return true;
-    }
-
-    return false;
-  }
-
-  this.getPollInterval = function() {
-    var value = self.getSetting('pollinterval');
-
-    if (value == null) {
-      return 5;
-    }
-
-    return value;
   }
 }
 
 (function($) {
-  var settings = new Settings;
-  var settingsManager = new SettingsManager(settings);
+  var settings = new Settings();
+  var pluginSettings = new PluginSettings(settings);
+  var settingsManager = new SettingsManager(pluginSettings);
   settingsManager.init();
 
-  $('input[name="beep"]').change(function() {
-    settingsManager.saveSetting('sound-notification', $(this).prop('checked'));
-  });
-
-  $('input[name="avatar"]').change(function() {
-    var checked = $(this).prop('checked');
-    settingsManager.saveSetting('avatar-notification', checked);
-    if (checked) {
-        $(':input[name="avataronload"]').removeAttr('disabled');
-    } else {
-        $(':input[name="avataronload"]').attr('disabled', true);
-    }
-  });
-
-  $('input[name="avataronload"]').change(function() {
-    settingsManager.saveSetting('avatar-onloadnotification', $(this).prop('checked'));
-  });
-
   $('input[name="icon"]').change(function() {
-    settingsManager.saveSetting('icon-enabled', $(this).prop('checked'));
+    settings.saveSetting('showIcon', $(this).prop('checked'));
   });
 
   $('input[name="oneboxcv"]').change(function() {
     var checked = $(this).prop('checked');
-    settingsManager.saveSetting('cv-onebox', checked);
+    settings.saveSetting('oneBox', checked);
+
     if (checked) {
-        $(':input[name^="oneboxheight"]').removeAttr('disabled');
+        $(':input[name="oneboxheight"]').removeAttr('disabled');
     } else {
-        $(':input[name^="oneboxheight"]').attr('disabled', true);
+        $(':input[name="oneboxheight"]').attr('disabled', true);
     }
-  });
-  $('input[name="oneboxcv"]').change();
-
-  $('input[name="oneboxdelv"]').change(function() {
-    settingsManager.saveSetting('delv-onebox', $(this).prop('checked'));
-  });
-
-  $('input[name="oneboxheight"]').keyup(function() {
-    settingsManager.saveSetting('onebox-height', $(this).val());
-  });
-
-  $('select[name="oneboxheightunits"]').change(function() {
-    settingsManager.saveSetting('onebox-heightunit', $(this).val());
-  });
-
-  $('input[name="status"]').change(function() {
-    var checked = $(this).prop('checked');
-    settingsManager.saveSetting('cv-status', $(this).prop('checked'));
-    if (checked) {
-        $(':input[name^="reason"]').removeAttr('disabled');
-        $(':input[name="poll"]').removeAttr('disabled');
-        if ($('input[name="poll"]').prop('checked')) {
-          $(':input[name="pollinterval"]').removeAttr('disabled');
-        }
-    } else {
-        $(':input[name^="reason"]').attr('disabled', true);
-        $(':input[name="poll"]').attr('disabled', true);
-        $(':input[name="pollinterval"]').attr('disabled', true);
-    }
-  });
-
-  $('input[name="reason"]').change(function() {
-    settingsManager.saveSetting('cv-reason', $(this).prop('checked'));
-  });
-
-  $('input[name="poll"]').change(function() {
-    var checked = $(this).prop('checked');
-    settingsManager.saveSetting('poll', checked);
-    if (checked) {
-        $(':input[name^="pollinterval"]').removeAttr('disabled');
-    } else {
-        $(':input[name^="pollinterval"]').attr('disabled', true);
-    }
-  });
-  $('input[name="poll"]').change();
-
-  $('input[name="pollinterval"]').keyup(function() {
-    settingsManager.saveSetting('pollinterval', $(this).val());
   });
 })(jQuery);
